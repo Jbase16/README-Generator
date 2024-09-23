28afb1a2d50c0b4b0e9a5edba34b056a550de979
Jbase16 <96518332+Jbase16@users.noreply.github.com>
2024-09-22 17:58:23 -0700

    Update generateMarkdown.js
    Adjusted logic flow and adjusted naming conventions.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,53 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === "MIT") {
-    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
-  } else if (license === "GNU GPLv3") {
-    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
-  } else if (license === "Apache 2.0") {
-    return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
-  } else if (license === "ISC") {
-    return "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)";
-  } else {
-    return "";
+// Function to get the appropriate badge for a license type
+// Returns an empty string if no valid license is provided
+function fetchLicenseBadge(licenseType) {
+  switch (licenseType) {
+    case "MIT":
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    case "GNU GPLv3":
+      return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+    case "Apache 2.0":
+      return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    case "ISC":
+      return "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)";
+    default:
+      return ""; // If no license or an unsupported one is provided
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "MIT") {
-    return "[MIT](https://opensource.org/licenses/MIT)";
-  } else if (license === "GNU GPLv3") {
-    return "[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)";
-  } else if (license === "Apache 2.0") {
-    return "[Apache 2.0](https://opensource.org/licenses/Apache-2.0)";
-  } else if (license === "ISC") {
-    return "[ISC](https://opensource.org/licenses/ISC)";
-  } else {
-    return "";
-  }
+// Function to get the corresponding license link
+// If no license is selected, returns an empty string
+function getLicenseLink(licenseType) {
+  const licenseLinks = {
+    "MIT": "[MIT](https://opensource.org/licenses/MIT)",
+    "GNU GPLv3": "[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)",
+    "Apache 2.0": "[Apache 2.0](https://opensource.org/licenses/Apache-2.0)",
+    "ISC": "[ISC](https://opensource.org/licenses/ISC)"
+  };
+
+  // Returning the correct link or empty if no match
+  return licenseLinks[licenseType] || "";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license) {
+// Function to add a license section to the README if a valid license exists
+function addLicenseSection(licenseType) {
+  if (licenseType && licenseType !== 'None') {
     return `## License
-This project is licensed under the ${license} license.`;
+This project is licensed under the ${licenseType} license.`;
   }
-  return "";
+  return ""; // No license section added if no valid license is provided
 }
 
-// TODO: Create a function to generate markdown for README
+// Function that creates the markdown structure for the README file
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const { title, description, installation, usage, contributing, tests, license, email, github } = data;
 
-${renderLicenseBadge(data.license)}
+  return `# ${title}
+
+${fetchLicenseBadge(license)}
 
 ## Description
-${data.description}
+${description}
 
 ## Table of Contents
 - [Installation](#installation)
@@ -55,25 +55,24 @@ ${data.description}
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
-${data.license !== 'None' ? '- [License](#license)' : ''}
+${license !== 'None' ? '- [License](#license)' : ''}
 
 ## Installation
-${data.installation}
+${installation}
 
 ## Usage
-${data.usage}
+${usage}
 
 ## Contributing
-${data.contributing}
+${contributing}
 
 ## Tests
-${data.tests}
+${tests}
 
-${renderLicenseSection(data.license)}
+${addLicenseSection(license)}
 
 ## Questions
-For any questions, please contact me at [${data.email}](mailto:${data.email}) or visit my GitHub profile at [https://github.com/${data.github}](https://github.com/${data.github}).
-
+If you have any questions, feel free to contact me by email at [${email}](mailto:${email}) or check out my GitHub profile: [https://github.com/${github}](https://github.com/${github}).
 `;
 }
 
